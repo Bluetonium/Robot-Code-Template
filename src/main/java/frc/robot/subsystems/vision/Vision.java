50154842454338b2a9c24dda0b80d8849a9b490f
PanzerLake96 <bennettjiang@fisdk12.org>
2026-09-08 14:18:34 -0500

    Fix import statement for BluetoniumSubsystemBase

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -9,7 +9,7 @@ import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import frc.Bluetonium.BluetoniumSubsystemBase
+import frc.Bluetonium.BluetoniumSubsystemBase;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.drivetrain.CommandSwerveDrivetrain;
 import frc.robot.subsystems.vision.VisionConstants.LIMELIGHTS;
